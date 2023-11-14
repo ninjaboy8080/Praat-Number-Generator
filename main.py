74937e38edb1507996005a50ad1ea3face85d983
Robert <robertjchen17@gmail.com>
2023-11-14 11:06:09 -0500

    Working version with streamlit upload/download

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import streamlit as st
+from io import TextIOWrapper
+from tempfile import NamedTemporaryFile
+import pandas
 def generate_number_tier(original_file):
 
     line_buffer = 0 # Prevents us from copying the tier metadata. The program wants to add lines
@@ -6,32 +9,35 @@ def generate_number_tier(original_file):
                     # don't want to copy. Namely, xmin, xmax, etc. There are 4 of these lines.
     new_file = [] # The original file, copied
     number_tier = [] # The new tier to be added, represented as a list of boundaries
-    with open(original_file,'r') as file:
-        token_count = {} # A dictionary of all the words and how many times each has occurred
-        word_tier = False # A boolean to track whether or not the program has read up to the word tier
-        for line_number, line in enumerate(file.readlines()):
-            line = line.strip()
-            new_file.append(line)
+    token_count = {} # A dictionary of all the words and how many times each has occurred
+    word_tier = False # A boolean to track whether or not the program has read up to the word tier
+    i = 0
+    for line_number, line in original_file.iterrows():
+        line = line['data']
+        line = line.strip()
+        if i == 2:
+            new_file.append("")
+        new_file.append(line)
+        i += 1
+        if line == "name = \"sentence - words\"":
+            word_tier = True
+        if word_tier:
+            line_buffer += 1
 
-            if line == "name = \"sentence - words\"":
-                word_tier = True
-            if word_tier:
-                line_buffer += 1
+            if line_buffer == 4:
+                interval_size = line[18:] # The number of boundaries in the sentence tier, and by extension,
+                                          # the number tier too.
 
-                if line_buffer == 4:
-                    interval_size = line[18:] # The number of boundaries in the sentence tier, and by extension,
-                                              # the number tier too.
-
-                if line_buffer > 4: # As mentioned above, there are 4 lines of metadata we want to skip.
-                    if "text" in line:
-                        token = line[8:-2]
-                        if token != "":
-                            if token not in token_count:
-                                token_count[token] = 1
-                            else:
-                                token_count[token] += 1
-                            line = "text = \"" + str(token_count[token]) + "\""
-                    number_tier.append(line)
+            if line_buffer > 4: # As mentioned above, there are 4 lines of metadata we want to skip.
+                if "text" in line:
+                    token = line[8:-2]
+                    if token != "":
+                        if token not in token_count:
+                            token_count[token] = 1
+                        else:
+                            token_count[token] += 1
+                        line = "text = \"" + str(token_count[token]) + "\""
+                number_tier.append(line)
     xmin = new_file[3][7:] # The xmin and xmax variables represent the length in seconds of the file. These are
     xmax = new_file[4][7:] # always on the 4th and 5th lines of the TextGrid, hence the use of indices 3 and 4.
     size = int(new_file[6][7:]) # Similarly, the size of the TextGrid represents the amount of tiers.
@@ -50,8 +56,11 @@ def generate_number_tier(original_file):
             file_2.write(line + "\n")
         for line in number_tier:
             file_2.write(line + "\n")
-    return file_2
-print(generate_number_tier("Input_test.TextGrid"))
+    with open("number_tier_output.txt", 'r') as output:
+        txt_convert = ""
+        for line in output.readlines():
+            txt_convert += line
+        return txt_convert
 header = st.container()
 body = st.container()
 file_upload = st.container()
@@ -69,11 +78,8 @@ with body:
             "corresponding to the amount of times that word has occured, starting from 1.")
 with file_upload:
     st.header("File input/output")
-    input_file = st.file_uploader("Upload file:")
+    input_file = st.file_uploader("Upload file:", type=["TextGrid"])
     if input_file is not None:
-        output_file = generate_number_tier(input_file)
-        st.download_button(output_file.getValue())
-    try:
-        output_file = generate_number_tier(input_file)
-    except:
-        print("No file found")
+        input_csv = pandas.read_csv(input_file, names=['data'])
+        output_file = generate_number_tier(input_csv)
+        st.download_button(label="Output Download",data=output_file,file_name="PraatNumGen_Output.TextGrid")

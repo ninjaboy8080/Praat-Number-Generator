5e5292d88b2346e5a493ba359dfe92f321c4fe39
Robert <robertjchen17@gmail.com>
2023-12-21 10:59:52 -0500

    Fixed last letter bug

--- main.py
+++ main.py
@@ -8,14 +8,10 @@ def generate_number_tier(original_file):
     number_tier = [] # The new tier to be added, represented as a list of boundaries
     token_count = {} # A dictionary of all the words and how many times each has occurred
     word_tier = False # A boolean to track whether or not the program has read up to the word tier
-    i = 0
     for line_number, line in original_file.iterrows():
         line = line['data']
         line = line.strip()
-        if i == 2:
-            new_file.append("")
         new_file.append(line)
-        i += 1
         if line == "name = \"sentence - words\"":
             word_tier = True
         if word_tier:
@@ -25,7 +21,7 @@ def generate_number_tier(original_file):
                                           # the number tier too.
             if line_buffer > 4: # As mentioned above, there are 4 lines of metadata we want to skip.
                 if "text" in line:
-                    token = line[8:-2]
+                    token = line[8:-1]
                     if token != "":
                         if token not in token_count:
                             token_count[token] = 1
@@ -33,6 +29,8 @@ def generate_number_tier(original_file):
                             token_count[token] += 1
                         line = "text = \"" + str(token_count[token]) + "\""
                 number_tier.append(line)
+    new_file.insert(2, "") # read_csv() seems to skip newlines. TextGrid files always have a newline for their
+                           # third line.
     xmin = new_file[3][7:] # The xmin and xmax variables represent the length in seconds of the file. These are
     xmax = new_file[4][7:] # always on the 4th and 5th lines of the TextGrid, hence the use of indices 3 and 4.
     size = int(new_file[6][7:]) # Similarly, the size of the TextGrid represents the amount of tiers.
@@ -52,6 +50,7 @@ def generate_number_tier(original_file):
     for line in number_tier:
         string_output += line + "\n"
     return string_output
+# Everything below is streamlit frontend
 header = st.container()
 body = st.container()
 file_upload = st.container()
@@ -66,7 +65,7 @@ with body:
             "will ignore empty intervals and create a new file, identical to the input file, but with\n"
             " an added Number tier. The Number tier will have the same boundaries as the sentence - \n"
             "words tier. For each interval with a word in it, the Number tier will contain a number \n"
-            "corresponding to the amount of times that word has occured, starting from 1.")
+            "corresponding to the amount of times that word has occurred, starting from 1.")
 with file_upload:
     st.header("File input/output")
     input_file = st.file_uploader("Upload file:", type=["TextGrid"])

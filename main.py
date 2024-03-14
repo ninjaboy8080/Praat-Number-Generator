9ebf72ad78e54715c36c91d28ce5ccdeb35116d5
Robert <robertjchen17@gmail.com>
2024-03-14 14:19:48 -0400

    Added alternate mode

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
 import streamlit as st
 import pandas
-def generate_number_tier(original_file):
+def get_word_category(word: str):
+    '''
+    This function returns an integer based on the "category" of the word. There are 4 categories that we
+    are concerned with. Affricate first, affricate second, fricative first, fricative second. "First" refers
+    to the phoneme being present in the first syllable. We need to treat these categories differently. For example,
+    take "hongcha", which is a affricate second word. We need to replace "C2c" with "Tc" and "C2r" with "T".
+    '''
+    # Here, we exploit the fact that all syllables in pinyin are at least two letters (for the words present
+    # in the word-list).
+    first_half = word[0:2]
+    second_half = word[2:]
+    if first_half == "ch":
+        return 1
+    if "ch" in second_half:
+        return 2
+    if first_half == "sh":
+        return 3
+    if "sh" in second_half:
+        return 4
+    return 0
+def generate_number_tier(original_file, mode='d'):
+    '''
+
+    :param original_file:
+    :param mode: Sets mode to [d]efault or [alt]ernate
+    :return:
+    '''
     line_buffer = 0 # Prevents us from copying the tier metadata. The program wants to add lines
                     # after reading the target tier name. However, there are lines underneath that we
                     # don't want to copy. Namely, xmin, xmax, etc. There are 4 of these lines.
     new_file = [] # The original file, copied
     number_tier = [] # The new tier to be added, represented as a list of boundaries
     token_count = {} # A dictionary of all the words and how many times each has occurred
+    if mode == "alt":
+        token_count = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0}
     word_tier = False # A boolean to track whether or not the program has read up to the word tier
-    for line_number, line in original_file.iterrows():
+    for _, line in original_file.iterrows():
         line = line['data']
         line = line.strip()
         new_file.append(line)
@@ -23,11 +51,16 @@ def generate_number_tier(original_file):
                 if "text" in line:
                     token = line[8:-1]
                     if token != "":
-                        if token not in token_count:
-                            token_count[token] = 1
-                        else:
-                            token_count[token] += 1
-                        line = "text = \"" + str(token_count[token]) + "\""
+                        if mode == "d":
+                            if token not in token_count:
+                                token_count[token] = 1
+                            else:
+                                token_count[token] += 1
+                            line = "text = \"" + str(token_count[token]) + "\""
+                        if mode == "alt":
+                            word_category = get_word_category(token)
+                            token_count[word_category] += 1
+                            line = "text = \"" + str(token_count[word_category]) + "\""
                 number_tier.append(line)
     new_file.insert(2, "") # read_csv() seems to skip newlines. TextGrid files always have a newline for their
                            # third line.
@@ -54,9 +87,10 @@ def generate_number_tier(original_file):
 header = st.container()
 body = st.container()
 file_upload = st.container()
+file_upload2 = st.container() # Necessary?
 with header:
     st.title("Welcome to the Praat Number Tier generator")
-    st.text("This program automatically count words in Praat.")
+    st.text("This program automatically counts words in Praat.")
 with body:
     st.header("How it works")
     st.text("The input must be a .TextGrid or .txt, with a tier titled \"sentence - words\" \n"
@@ -73,3 +107,10 @@ with file_upload:
         input_csv = pandas.read_csv(input_file, names=['data'])
         output_file = generate_number_tier(input_csv)
         st.download_button(label="Output Download",data=output_file,file_name="PraatNumGen_Output.TextGrid")
+with file_upload2:
+    st.header("Alternate file input/output")
+    input_file = st.file_uploader("Upload file:", type=["TextGrid"])
+    if input_file is not None:
+        input_csv = pandas.read_csv(input_file, names=['data'])
+        output_file = generate_number_tier(input_csv, mode='alt')
+        st.download_button(label="Output Download", data=output_file, file_name="PraatNumGen_Output.TextGrid")
